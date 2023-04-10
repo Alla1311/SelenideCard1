337045c8e756fd28b9b901313f07aca02aa28914
Alla <lialia13111989@mail.ru>
2023-04-10 21:48:06 +0300

    initial commit

---
+++ src/test/java/SelenideCardTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideCardTest {
+
+   public String localDate (int day) {
+        LocalDate date = LocalDate.now();
+        String plusDays = String.valueOf(LocalDate.now());
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldDeliveryCard(){
+        open("http://localhost:9999/");
+        $("[data-test-id='city' ] input").setValue("Нижний Новгород");
+        $("[data-test-id='date'] input").setValue(localDate(7);
+        $("[data-test-id='name'] input").setValue("Иванов-Сидоров Иван");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15).plusDays(text(localDate(7))));
+
+    }
+
+}
